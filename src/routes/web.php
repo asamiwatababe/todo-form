d0969eea7a0e835455be60118797ce865a634191
root <root@DESKTOP-LBQNHPE.>
2025-04-17 16:42:32 +0900

    更新と削除機能を実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,6 @@ use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index']);
 Route::post('/todos', [TodoController::class, 'store']);
+Route::post('/todos/update' ,[TodoController::class,'update']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy']);
